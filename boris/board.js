5e06fb772fa81f6465c5eea459dbbad681142776
Mark Steadman <mark@podiant.co>
2019-10-02 11:47:49 +0100

    Adds Boris and Bohemian Rhapsody boards

---
+++ boris/board.js
@@ -0,0 +1,127 @@
+initBoard(
+    {
+        "title": "The Boris Johnson Soundboard",
+        "sounds": [
+            {
+                "title": "1, 2, 3",
+                "url": "01.mp3",
+                "thumbnail": "01.jpg"
+            },
+            {
+                "title": "And and and",
+                "url": "02.mp3",
+                "thumbnail": "02.jpg"
+            },
+            {
+                "title": "Argie bargie",
+                "url": "03.mp3",
+                "thumbnail": "03.jpg"
+            },
+            {
+                "title": "Assange",
+                "url": "04.mp3",
+                "thumbnail": "04.jpg"
+            },
+            {
+                "title": "Baby",
+                "url": "05.mp3",
+                "thumbnail": "05.jpg"
+            },
+            {
+                "title": "Belgium",
+                "url": "06.mp3",
+                "thumbnail": "06.jpg"
+            },
+            {
+                "title": "Boo!",
+                "url": "07.mp3",
+                "thumbnail": "07.jpg"
+            },
+            {
+                "title": "I think I'm a cat",
+                "url": "08.mp3",
+                "thumbnail": "08.jpg"
+            },
+            {
+                "title": "Let's be absolutely clear",
+                "url": "09.mp3",
+                "thumbnail": "09.jpg"
+            },
+            {
+                "title": "Cornflakes",
+                "url": "10.mp3",
+                "thumbnail": "10.jpg"
+            },
+            {
+                "title": "England",
+                "url": "11.mp3",
+                "thumbnail": "11.jpg"
+            },
+            {
+                "title": "Hands",
+                "url": "12.mp3",
+                "thumbnail": "12.jpg"
+            },
+            {
+                "title": "Hat boat, hat boat",
+                "url": "13.mp3",
+                "thumbnail": "13.jpg"
+            },
+            {
+                "title": "If I may",
+                "url": "14.mp3",
+                "thumbnail": "14.jpg"
+            },
+            {
+                "title": "Jellies",
+                "url": "15.mp3",
+                "thumbnail": "15.jpg"
+            },
+            {
+                "title": "Mmmm",
+                "url": "16.mp3",
+                "thumbnail": "16.jpg"
+            },
+            {
+                "title": "No",
+                "url": "17.mp3",
+                "thumbnail": "17.jpg"
+            },
+            {
+                "title": "Reincarnated as an olive",
+                "url": "18.mp3",
+                "thumbnail": "18.jpg"
+            },
+            {
+                "title": "Oory oory oog oog",
+                "url": "19.mp3",
+                "thumbnail": "19.jpg"
+            },
+            {
+                "title": "Supercolossal pickled onion",
+                "url": "20.mp3",
+                "thumbnail": "20.jpg"
+            },
+            {
+                "title": "So so so",
+                "url": "21.mp3",
+                "thumbnail": "21.jpg"
+            },
+            {
+                "title": "Why not?",
+                "url": "22.mp3",
+                "thumbnail": "22.jpg"
+            },
+            {
+                "title": "Churchill",
+                "url": "23.mp3",
+                "thumbnail": "23.jpg"
+            },
+            {
+                "title": "Yes",
+                "url": "24.mp3",
+                "thumbnail": "24.jpg"
+            }
+        ]
+    }
+);
---
+++ rhapsody/board.js
@@ -0,0 +1,95 @@
+initBoard(
+    {
+        "title": "The Bohemian Rhapsody Soundboard",
+        "sounds": [
+            {
+                "title": "I see a little silhouetto of a man",
+                "url": "01.mp3"
+            },
+            {
+                "title": "Scaramouche, Scaramouche, will you do the Fandango?",
+                "url": "02.mp3"
+            },
+            {
+                "title": "Thunderbolt and lightning, very, very frightening me",
+                "url": "03.mp3"
+            },
+            {
+                "title": "Galileo",
+                "url": "04.mp3"
+            },
+            {
+                "title": "Galileo",
+                "url": "05.mp3"
+            },
+            {
+                "title": "Galileo Figaro",
+                "url": "06.mp3"
+            },
+            {
+                "title": "Magnifico!",
+                "url": "07.mp3"
+            },
+            {
+                "title": "I'm just a poor boy, nobody loves me",
+                "url": "08.mp3"
+            },
+            {
+                "title": "He's just a poor boy from a poor family, spare him his life from this monstrosity",
+                "url": "09.mp3"
+            },
+            {
+                "title": "Easy come, easy go, will you let me go?",
+                "url": "10.mp3"
+            },
+            {
+                "title": "Bismillah!",
+                "url": "11.mp3"
+            },
+            {
+                "title": "No, we will not let you go",
+                "url": "12.mp3"
+            },
+            {
+                "title": "Let him go!",
+                "url": "13.mp3"
+            },
+            {
+                "title": "We will not let you go",
+                "url": "14.mp3"
+            },
+            {
+                "title": "Will not let you go",
+                "url": "15.mp3"
+            },
+            {
+                "title": "Let me go...",
+                "url": "16.mp3"
+            },
+            {
+                "title": "No no no no no no no!",
+                "url": "17.mp3"
+            },
+            {
+                "title": "Oh mama mia, mama mia",
+                "url": "18.mp3"
+            },
+            {
+                "title": "Mama mia let me go",
+                "url": "19.mp3"
+            },
+            {
+                "title": "Beelzebub has a devil put aside for me",
+                "url": "20.mp3"
+            },
+            {
+                "title": "For me",
+                "url": "21.mp3"
+            },
+            {
+                "title": "For me!",
+                "url": "22.mp3"
+            }
+        ]
+    }
+);
